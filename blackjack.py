e23e8f5a15542b733ecca3f49ff17bcd8debd382
Matota <72810000+mat0ta@users.noreply.github.com>
2021-11-16 12:15:53 +0100

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,19 @@
-import random
-from random import choice, sample, shuffle
+from random import *
+from itertools import *
+from tkinter import *
 
-cartas = [chr(x) for x in range(0x1f0a1, 0x1f0af)]
-
-print(choice(cartas))
+cartas = { 
+    chr(0x1f0a1): 11, 
+    chr(0x1f0a2): 2, 
+    chr(0x1f0a3): 3, 
+    chr(0x1f0a4): 4, 
+    chr(0x1f0a5): 5, 
+    chr(0x1f0a6): 6, 
+    chr(0x1f0a7): 7, 
+    chr(0x1f0a8): 8, 
+    chr(0x1f0a9): 9, 
+    chr(0x1f0aa): 10, 
+    chr(0x1f0ab): 10, 
+    chr(0x1f0ad): 10, 
+    chr(0x1f0ae): 10, 
+} 
