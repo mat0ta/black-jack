9fc447ebd453396bdbf4c03385719e090bb3c0c5
Matota <72810000+mat0ta@users.noreply.github.com>
2021-11-20 21:07:08 +0100

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -20,18 +20,35 @@ cartas = {
 class blackjack():
     def __init__(self):
         self.mano = []
-        self.banca = []
+        self.dealer = []
         self.baraja = list(cartas.keys()) * 4
     def darCartas(self, cantidad, mano):
         for i in range(cantidad):
             shuffle(self.baraja)
             card = self.baraja.pop()
             mano.append(card)
+    def calcularMano(self, mano):
+        puntos = 0
+        for i in range(len(mano)):
+            try:
+                puntos += cartas[mano[i]]
+                return puntos
+            except:
+                return print('Ha ocurrido un error.')
+    def iniciarJuego(self):
+        print('¡Bienvenido al Blackjack!\nA continuacion el Dealer te asignara tu mano.')
+        blackjack().darCartas(2, self.mano)
+        blackjack().darCartas(1, self.dealer)
+        print('Tu mano:\n' + self.mano[0] + ' ' + self.mano[1] + '\nPuntos: ' + str(blackjack().calcularMano(self.mano)) + '\n\nDealer:\n' + self.dealer[0] + '\nPuntos: ' + str(blackjack().calcularMano(self.dealer)))
+    def hit(self):
+        print('Hit')
+    def stand(self):
+        print('Stand')
     def testDef(self):
         blackjack().darMano(2, self.mano)
 # print(cartas[baraja[1]])
 
-blackjack().testDef()
+blackjack().iniciarJuego()
 
 # print(baraja)
 
